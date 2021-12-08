44c81550ca415a0e35f2eaf202d4032b53f33ea4
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-12-08 21:32:57 +0100

    upload zip file to tmp instead of home

--- functions/uploadUserData.m
+++ functions/uploadUserData.m
@@ -8,7 +8,7 @@ function uploadUserData(ps)
 
 %% ssh2 config
 ssh2_conn = ssh2_config(ps.hostname, ps.username, ps.password);         % configure ssh2 connection
-ssh2_conn = ssh2_command(ssh2_conn, 'ls -la *ninjas*');
+ssh2_conn = ssh2_command(ssh2_conn, ['mkdir -p /tmp/', ps.username]);
 
 %% zip dir to upload
 dummyFilenameZip = ['upload', num2str(randi([1000, 9999], 1))];
@@ -18,16 +18,17 @@ zip(dummyFilenameZip, listing);
 
 %% upload zip file and delete local zip file
 ssh2_conn = sftp_put(ssh2_conn, ...                                     % upload zip file
-     [dummyFilenameZip, '.zip']);
+     [dummyFilenameZip, '.zip'], ...
+     ['/tmp/', ps.username]);
 delete([dummyFilenameZip, '.zip']);                                     % delete local zip file
 cd(ps.locPath);                                                         % change dir back to local home dir
 
 %% unzip uploaded zip-file on server
 ssh2_conn = ssh2_command(ssh2_conn, ['mkdir -p ' ps.extUploadFolderConcrete]);
 ssh2_conn = ssh2_command(ssh2_conn, ...                                 % unzip uploadesd zip file to new dir
-    ['unzip ', dummyFilenameZip, '.zip -d ', ps.extUploadFolderConcrete]);
+    ['unzip ', '/tmp/', ps.username,'/', dummyFilenameZip, '.zip -d ', ps.extUploadFolderConcrete]);
 ssh2_conn = ssh2_command(ssh2_conn, ...                                 % delete zip file
-    ['rm -rf ', dummyFilenameZip, '.zip']);
+    ['rm -rf ', '/tmp/', ps.username,'/', dummyFilenameZip, '.zip']);
 
 %% close ssh2 connection
 ssh2_conn = ssh2_close(ssh2_conn);
